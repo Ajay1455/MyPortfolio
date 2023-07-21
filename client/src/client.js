6c21e538af36226d623cdf7f7b21c257a9136fcb
Ajay1455 <75538701+Ajay1455@users.noreply.github.com>
2023-07-21 15:55:30 +0530

    Update client.js

--- client/src/client.js
+++ client/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.SPID,
+  projectId: process.env.REACT_APP_SPID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.STOKEN,
+  token: process.env.REACT_APP_STOKEN,
 });
 
 const builder = imageUrlBuilder(client);
